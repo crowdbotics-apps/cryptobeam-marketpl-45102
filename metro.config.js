78338e4b7b964eac3a3fa0a60e4ae4dd8d11930b
Crowdbotics <team@crowdbotics.com>
2023-12-19 16:06:10 +0000

    Initial commit

---
+++ metro.config.js
@@ -0,0 +1,48 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+const path = require("path")
+
+const extraNodeModules = {
+  "@components": path.resolve(__dirname, "components"),
+  "@modules": path.resolve(__dirname, "modules"),
+  "@screens": path.resolve(__dirname, "screens"),
+  "@options": path.resolve(__dirname, "options"),
+  "@store": path.resolve(__dirname, "store"),
+  "@helpers": path.resolve(__dirname, "helpers")
+}
+
+const watchFolders = [
+  path.resolve(__dirname, "components"),
+  path.resolve(__dirname, "modules"),
+  path.resolve(__dirname, "screens"),
+  path.resolve(__dirname, "options"),
+  path.resolve(__dirname, "store"),
+  path.resolve(__dirname, "helpers")
+]
+
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false
+      }
+    })
+  },
+  resolver: {
+    sourceExts: ["js", "jsx", "ts", "tsx", "json"],
+    extraNodeModules: new Proxy(extraNodeModules, {
+      get: (target, name) =>
+        //redirects dependencies referenced from extraNodeModules to local node_modules
+        name in target
+          ? target[name]
+          : path.join(process.cwd(), "node_modules", name)
+    })
+  },
+  watchFolders,
+  resetCache: true
+}
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "old-boat-45102",
+  url: "https://old-boat-45102.botics.co",
+  api: "https://old-boat-45102.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
