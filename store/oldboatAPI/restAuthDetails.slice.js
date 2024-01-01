fe1c179a247dddaa8e7bbaf7b966707fe485f77c
Crowdbotics <engineering@crowdbotics.com>
2024-01-01 20:50:22 +0000

    Update store.

--- store/oldboatAPI/restAuthDetails.slice.js
+++ store/oldboatAPI/restAuthDetails.slice.js
@@ -7,17 +7,17 @@ export const rest_auth_logout_create = createAsyncThunk(
     return response.data
   }
 )
-export const rest_auth_password_change_create = createAsyncThunk(
-  "restAuthDetails/rest_auth_password_change_create",
-  async payload => {
-    const response = await apiService.rest_auth_password_change_create(payload)
-    return response.data
-  }
-)
 export const rest_auth_password_reset_create = createAsyncThunk(
   "restAuthDetails/rest_auth_password_reset_create",
   async payload => {
     const response = await apiService.rest_auth_password_reset_create(payload)
+    return response.data
+  }
+)
+export const rest_auth_password_change_create = createAsyncThunk(
+  "restAuthDetails/rest_auth_password_change_create",
+  async payload => {
+    const response = await apiService.rest_auth_password_change_create(payload)
     return response.data
   }
 )
@@ -72,23 +72,6 @@ const restAuthDetailsSlice = createSlice({
           state.api.loading = "idle"
         }
       })
-      .addCase(rest_auth_password_change_create.pending, (state, action) => {
-        if (state.api.loading === "idle") {
-          state.api.loading = "pending"
-        }
-      })
-      .addCase(rest_auth_password_change_create.fulfilled, (state, action) => {
-        if (state.api.loading === "pending") {
-          state.entities.push(action.payload)
-          state.api.loading = "idle"
-        }
-      })
-      .addCase(rest_auth_password_change_create.rejected, (state, action) => {
-        if (state.api.loading === "pending") {
-          state.api.error = action.error
-          state.api.loading = "idle"
-        }
-      })
       .addCase(rest_auth_password_reset_create.pending, (state, action) => {
         if (state.api.loading === "idle") {
           state.api.loading = "pending"
@@ -101,6 +84,23 @@ const restAuthDetailsSlice = createSlice({
         }
       })
       .addCase(rest_auth_password_reset_create.rejected, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.api.error = action.error
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(rest_auth_password_change_create.pending, (state, action) => {
+        if (state.api.loading === "idle") {
+          state.api.loading = "pending"
+        }
+      })
+      .addCase(rest_auth_password_change_create.fulfilled, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.entities.push(action.payload)
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(rest_auth_password_change_create.rejected, (state, action) => {
         if (state.api.loading === "pending") {
           state.api.error = action.error
           state.api.loading = "idle"
@@ -188,8 +188,8 @@ const restAuthDetailsSlice = createSlice({
 })
 export default {
   rest_auth_logout_create,
-  rest_auth_password_change_create,
   rest_auth_password_reset_create,
+  rest_auth_password_change_create,
   rest_auth_password_reset_confirm_create,
   rest_auth_registration_resend_email_create,
   rest_auth_registration_verify_email_create,
